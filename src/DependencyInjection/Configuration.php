eb802b74c0940b18d8429ba2c069bc3e18eb933e
sarunas <sarunasval@gmail.com>
2019-08-07 12:41:26 +0300

    Initial commit

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlumTreeSystems\SyliusBlogPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('plum_tree_systems_sylius_blog_plugin');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('plum_tree_systems_sylius_blog_plugin');
+        }
+
+        return $treeBuilder;
+    }
+}
---
+++ src/PlumTreeSystemsSyliusBlogPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlumTreeSystems\SyliusBlogPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class PlumTreeSystemsSyliusBlogPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
